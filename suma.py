4247e119aa0e464ad9781ad43c827299ec0804ed
Juan García Montero <jgmen20@gmail.com>
2024-02-06 19:39:51 +0100

    Agrego operaciones de restar, multiplicar y dividir; test y receta Actions

--- suma.py
+++ suma.py
@@ -1,4 +1,4 @@
-# sumar.py
+# calculadora.py
 
 def sumar(a, b):
     return a + b
---
+++ test_calculadora.py
@@ -0,0 +1,22 @@
+import unittest
+from calculadora import sumar, restar, multiplicar, dividir
+
+class TestCalculadora(unittest.TestCase):
+    def test_calculadora(self):
+        self.assertEqual(sumar(3, 2), 5)
+        self.assertEqual(sumar(-1, 1), 0)
+        self.assertEqual(sumar(-1, -1), -2)
+        self.assertEqual(restar(3, 2), 1)
+        self.assertEqual(restar(-1, 1), -2)
+        self.assertEqual(restar(-1, -1), 0)
+        self.assertEqual(multiplicar(3, 2), 6)
+        self.assertEqual(multiplicar(-1, 1), -1)
+        self.assertEqual(multiplicar(-1, -1), 1)
+        self.assertEqual(dividir(3, 2), 1.5)
+        self.assertEqual(dividir(-1, 1), -1)
+        self.assertEqual(dividir(-1, -1), 1)
+        self.assertEqual(dividir(5, 0), 'No se puede dividir por 0')
+
+
+if __name__ == '__main__':
+    unittest.main()
