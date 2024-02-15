73b086fec292f7a8d3a08a061f28ddc3a4a799b2
Juan García Montero <jgmen20@gmail.com>
2024-02-15 14:38:02 +0100

    Cambios en calculadora . main

--- calculadora.py
+++ calculadora.py
@@ -11,7 +11,7 @@ def multiplicar(a, b):
 
 def dividir(a, b):
     if b == 0:
-        return "No se puede dividir por 0"
+        return "No se puede dividir por 0."
     else:
         return a / b
 
